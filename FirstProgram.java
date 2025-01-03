55ac5e18207c6e5dbec91b6fff160405f67467a5
Allan Cheruiyot <allankipkoech9@gmail.com>
2025-01-03 13:53:17 +0300

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+
+        
+        System.out.print("Hello world ");
+        System.out.print("One");
+    }
+}
---
+++ getAge.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class getAge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        System.out.println("Enter your age:");
+        age = scanner.nextInt();
+        System.out.println("Your age is: " + age);
+      
+    }
+    
+}
